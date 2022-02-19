865140c86fc533907b1321accb015baae4e38de7
Hamza <hamzajaved091@gmail.com>
2022-02-19 13:49:40 +0500

    Consumer UI

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+// import { ProductsContext } from "../context/ProductsContext";
+// import ProductInfo from "./ProductInfo";
+import SideBar from "./SideBar";
+import "./HomeStyle.css";
+
+const Home = (props) => {
+  // const { getProductById } = useContext(ProductsContext);
+  const [productInfo, setProductInfo] = useState(null);
+  let location = useLocation();
+
+  useEffect(() => {
+    if (location.state) {
+      const product = getProductById(location.state.qrId);
+      product.then((prod) => setProductInfo(prod));
+      // setProductInfo(product);
+    }
+  }, []);
+
+  return (
+    <>
+      <SideBar camera={false} />
+      <Link className="HomeQRLink" to="/qr-scan">
+        <button className="HomeQRButton">Scan QR code</button>
+      </Link>
+      {/* <ProductInfo productInfo={productInfo} /> */}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState, useContext } from "react";
+import { ProductsContext } from "../context/ProductsContext";
+import ProductInfo from "./ProductInfo";
+import "./HomeStyle.css";
+
+const SideBar = ({ camera, qrId }) => {
+  const [productInfo, setProductInfo] = useState(null);
+  const { getProductById } = useContext(ProductsContext);
+  //   console.log(qrId);
+
+  useEffect(() => {
+    const product = getProductById(qrId);
+    product.then((prod) => setProductInfo(prod));
+    // setProductInfo(product);
+  }, [qrId]);
+  return (
+    <div className="sidebar">
+      <section className="cameras">
+        <h2>Cameras</h2>
+        <ul>
+          {camera ? (
+            <li className="active">Integrated Camera</li>
+          ) : (
+            <li className="empty">No Cameras found</li>
+          )}
+        </ul>
+      </section>
+      <section className="scans">
+        <h2>Scans</h2>
+        <ul>
+          {!qrId ? (
+            <li className="empty">No Scan found</li>
+          ) : (
+            <li className="scans-enter">{qrId}</li>
+          )}
+        </ul>
+      </section>
+      <section className="items">
+        <h2>Product Information</h2>
+        <ProductInfo productInfo={productInfo} />
+      </section>
+    </div>
+  );
+};
+
+export default SideBar;
